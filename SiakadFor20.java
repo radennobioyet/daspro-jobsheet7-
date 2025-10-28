948935c16ac4000dce2a947cb18cb8ce08d60fb1
“radennobioyetgit config --global user.name “radennobioyet <radensnpmb@gmail.com>
2025-10-28 20:34:42 +0700

    edit

--- SiakadFor20.java
+++ SiakadFor20.java
@@ -5,6 +5,7 @@ public class SiakadFor20 {
         Scanner sc = new Scanner(System.in);
 
         double nilai, tertinggi = 0, terendah = 100;
+        int lulus=0, tidakLulus=0;
 
         for (int i = 1; i <= 10; i++) {
             System.out.print("Masukkan Nilai Mahasiswa ke-" + i + ": ");
@@ -15,10 +16,17 @@ public class SiakadFor20 {
             if (nilai < terendah) {
                 terendah = nilai;
             }
+
+            if (nilai >=60) {
+                lulus++;
+            } else {
+                tidakLulus++;
+            }
         }
 
         System.out.println("Nilai tertinggi : " + tertinggi);
         System.out.println("Nilai terendah : " + terendah);
-
+        System.out.println("Jumlah Mahasiswa lulus : " + lulus);
+        System.out.println("Jumlah Mahasiswa tidak lulus : " + tidakLulus);
     }
 }
