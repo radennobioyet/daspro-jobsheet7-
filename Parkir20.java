982f1901cdcec26407254320f93c031ec53fc2b6
“radennobioyetgit config --global user.name “radennobioyet <radensnpmb@gmail.com>
2025-10-28 21:08:20 +0700

    TUGAS 2

---
+++ Parkir20.java
@@ -0,0 +1,30 @@
+import java.util.Scanner;
+
+public class Parkir20 {
+    public static void main(String[] args) {
+        Scanner input = new Scanner(System.in);
+        int jenis, durasi;
+        int total = 0;
+
+        do {
+            System.out.print("Masukkan jenis kendaraan (1: Mobil, 2: Motor, 0: Keluar): ");
+            jenis = input.nextInt();
+
+            if (jenis == 1 || jenis == 2) {
+                System.out.print("Masukkan durasi parkir (jam): ");
+                durasi = input.nextInt();
+
+                if (durasi > 5) {
+                    total += 12500;
+                } else if (jenis == 1) {
+                    total += durasi * 3000;
+                } else if (jenis == 2) {
+                    total += durasi * 2000;
+                }
+            }
+        } while (jenis != 0);
+
+        System.out.println("Total pembayaran parkir: Rp " + total);
+        input.close();
+    }
+}
